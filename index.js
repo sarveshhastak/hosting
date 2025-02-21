b252663be794c1a38906175b2a17a1366ac7a59d
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-02-21 15:02:33 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 const app = express()
 app.use(express.json()) //👈stores body to req.body
@@ -8,12 +9,13 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
-
+app.use(express.static("dist"))
 //                    go to this 👇 if this url has this =>http://localhost:5000/api/blog
 app.use("/api/blog", require("./blog.route"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "404 resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "404 resource not found" })
 })
 
 
